9e3275f1ffad00b1dd192484877981aaa7d247a4
LAPTOP-JR8Q3D6N\Itec Global Services <fode.oumar@gmaail.com>
2023-02-19 16:05:31 +0100

    Mise à jour du script optimisé

--- optimized.py
+++ optimized.py
@@ -1,5 +1,6 @@
 # Importation des modules nécessaires
 import numpy as np
+import csv
 
 # Définition de la liste des actions et de leurs coûts et bénéfices
 actions = [("Action-1", 20, 0.05), ("Action-2", 30, 0.1), ("Action-3", 50, 0.15),
@@ -9,6 +10,34 @@ actions = [("Action-1", 20, 0.05), ("Action-2", 30, 0.1), ("Action-3", 50, 0.15)
            ("Action-13", 38, 0.23), ("Action-14", 14, 0.01), ("Action-15", 18, 0.03),
            ("Action-16", 8, 0.08), ("Action-17", 4, 0.12), ("Action-18", 10, 0.14),
            ("Action-19", 24, 0.21), ("Action-20", 114, 0.18)]
+
+
+def csv_to_list(csv_file):
+    """
+    Fonction copiant les données d'un fichier csv vers une liste, et efface les données
+    erronées (inférieur ou égal à 0).
+    """
+    with open(csv_file) as dataset:
+        dataset_read = csv.reader(dataset)
+
+        dataset0 = []
+
+        for row in dataset_read:
+            dataset0.append(row)
+
+    dataset0.pop(0)
+
+    clean_dataset = []
+
+    for row in dataset0:
+
+        row[2] = int(round(float(row[1]) * (float(row[2]) / 100), 2) * 100)
+        row[1] = int(float(row[1]) * 100)
+
+        if row[1] > 0 and row[2] > 0:
+            clean_dataset.append(row)
+
+    return clean_dataset
 
 
 def trouver_actions_optimales(actions, max_cout):
@@ -49,4 +78,35 @@ def trouver_actions_optimales(actions, max_cout):
     print(f"avec ces actions: {[num_actions[0] for num_actions in lst_actions_selectionnes]}")
 
 
-trouver_actions_optimales(actions, 500)
+def dynamic_compare_to_siena_data(infile_siena, csv_file):
+    """
+    Application des fonctions précédente sur les données de Sienna et on renvoie les résultats
+    """
+    list_siena = []
+
+    for ligne in open(infile_siena):
+        selection = ligne.find("Share")
+        if selection == 0:
+            action = slice(10)
+            list_siena.append(ligne[action])
+
+    print(list_siena)
+
+    dataset = csv_to_list(csv_file)
+
+    dataset_compare = []
+
+    for i in dataset:
+        for j in list_siena:
+            if i[0] == j:
+                dataset_compare.append(i)
+
+    return trouver_actions_optimales(dataset_compare, 50000)
+
+
+#trouver_actions_optimales(csv_to_list("dataset1_Python+P7.csv"), 5000)
+#print(trouver_actions_optimales(csv_to_list('dataset2_Python+P7.csv'), 5000))
+
+print(dynamic_compare_to_siena_data('solution1_Python+P7siena.txt', 'dataset1_Python+P7.csv'))
+#print(dynamic_compare_to_siena_data('solution2_Python+P7siena.txt', 'dataset2_Python+P7.csv'))
+
